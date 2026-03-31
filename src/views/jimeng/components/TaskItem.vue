a780c35a0c74b81046171ac1508fb2dd7b49951b
Tokgo180313 <zyh180313@163.com>
2026-03-31 16:28:09 +0800

    2026-03-31: 调整压缩参数

--- src/views/jimeng/components/TaskItem.vue
+++ src/views/jimeng/components/TaskItem.vue
@@ -353,7 +353,7 @@ async function handleChange(
     }
   }
   if (first?.raw && !first.base64) {
-    const maxBytes = 2 * 1024 * 1024;
+    const maxBytes = 1 * 1024 * 1024;
 
     let rawFile = first.raw as File;
 
@@ -380,7 +380,7 @@ async function handleChange(
 
     if (rawFile.size > maxBytes) {
       rawFile = (await imageCompression(rawFile, {
-        maxSizeMB: 2,
+        maxSizeMB: 1,
         initialQuality: 1,
         maxIteration: 10,
         useWebWorker: true,
